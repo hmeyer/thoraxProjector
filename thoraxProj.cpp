f836f1677724e50a0f24d72f8607eab97ce807ea
Henning Meyer <Henning.Meyer@uhn.on.ca>
2010-05-03 12:01:20 -0400

    now outputs dicom

--- thoraxProj.cpp
+++ thoraxProj.cpp
@@ -1,18 +1,12 @@
-#include "itkImage.h"
-#include "itkImageFileReader.h"
-#include "itkImageFileWriter.h"
 #include "itkResampleImageFilter.h"
-#include "itkCenteredEuler3DTransform.h"
-#include "itkNearestNeighborInterpolateImageFunction.h"
-#include "itkImageRegionIterator.h"
-#include "itkCastImageFilter.h"
-
 #include "itkSigmoidImageFilter.h"
 
 #include "itkbasics.h"
 
-#include "itkRayCastInterpolateImageFunction.h"
-
+#include "itkAccumulateImageFilter.h"
+#include <itkExtractImageFilter.h>
+#include <itkCenteredRigid2DTransform.h>
+#include <itkBSplineInterpolateImageFunction.h>
 #include <boost/filesystem/operations.hpp>
 #include <boost/format.hpp>
 
@@ -20,222 +14,28 @@
 using std::string;
 
 
-void usage()
-{
-  std::cerr << "\n";
-  std::cerr << "Usage: DRR <options> [inputdir]\n";
-  std::cerr << "       calculates the Digitally Reconstructed Radiograph from  \n";
-  std::cerr << "       a volume. \n\n";
-  std::cerr << "   where <options> is one or more of the following:\n\n";
-  std::cerr << "       <-h>                    Display (this) usage information\n";
-  std::cerr << "       <-v>                    Verbose output [default: no]\n";
-  std::cerr << "       <-res float float>      Pixel spacing of the output image [default: 1x1mm]  \n";
-  std::cerr << "       <-size int int>         Dimension of the output image [default: 501x501]  \n";
-  std::cerr << "       <-sid float>            Distance of ray source (focal point) [default: 400mm]\n";
-  std::cerr << "       <-t float float float>  Translation parameter of the camera \n";
-  std::cerr << "       <-rx float>             Rotation around x,y,z axis in degrees \n";
-  std::cerr << "       <-ry float>\n";
-  std::cerr << "       <-rz float>\n";
-  std::cerr << "       <-normal float float>   The 2D projection normal position [default: 0x0mm]\n";
-  std::cerr << "       <-cor float float float> The centre of rotation relative to centre of volume\n";
-  std::cerr << "       <-threshold float>      Threshold [default: 0]\n";
-  std::cerr << "       <-o file>               Output image filename\n\n";
-  std::cerr << "                               by  thomas@hartkens.de\n";
-  std::cerr << "                               and john.hipwell@kcl.ac.uk (CISG London)\n\n";
-  exit(1);
-}
-
 int main( int argc, char *argv[] )
 {
-  char *input_name = NULL;
-  char *output_name = NULL;
-
-  bool ok;
-  bool verbose = false;
-
-  float rx = 0.;
-  float ry = 0.;
-  float rz = 0.;
-
-  float tx = 0.;
-  float ty = 0.;
-  float tz = 0.;
-
-  float cx = 0.;
-  float cy = 0.;
-  float cz = 0.;
-
-  float sid = 400.;
-
-  float sx = 1.;
-  float sy = 1.;
-
-  int dx = 501;
-  int dy = 501;
-
-  float o2Dx = 0;
-  float o2Dy = 0;
-
-  double threshold=0;
-
-
-  // Parse command line parameters
-
-  while (argc > 1)
-    {
-    ok = false;
-
-    if ((ok == false) && (strcmp(argv[1], "-h") == 0))
-      {
-      argc--; argv++;
-      ok = true;
-      usage();
-      }
-
-    if ((ok == false) && (strcmp(argv[1], "-v") == 0))
-      {
-      argc--; argv++;
-      ok = true;
-      verbose = true;
-      }
-
-    if ((ok == false) && (strcmp(argv[1], "-rx") == 0))
-      {
-      argc--; argv++;
-      ok = true;
-      rx=atof(argv[1]);
-      argc--; argv++;
-      }
-
-    if ((ok == false) && (strcmp(argv[1], "-ry") == 0))
-      {
-      argc--; argv++;
-      ok = true;
-      ry=atof(argv[1]);
-      argc--; argv++;
-      }
-
-    if ((ok == false) && (strcmp(argv[1], "-rz") == 0))
-      {
-      argc--; argv++;
-      ok = true;
-      rz=atof(argv[1]);
-      argc--; argv++;
-      }
-
-    if ((ok == false) && (strcmp(argv[1], "-threshold") == 0))
-      {
-      argc--; argv++;
-      ok = true;
-      threshold=atof(argv[1]);
-      argc--; argv++;
-      }
-
-    if ((ok == false) && (strcmp(argv[1], "-t") == 0))
-      {
-      argc--; argv++;
-      ok = true;
-      tx=atof(argv[1]);
-      argc--; argv++;
-      ty=atof(argv[1]);
-      argc--; argv++;
-      tz=atof(argv[1]);
-      argc--; argv++;
-      }
-
-    if ((ok == false) && (strcmp(argv[1], "-cor") == 0))
-      {
-      argc--; argv++;
-      ok = true;
-      cx=atof(argv[1]);
-      argc--; argv++;
-      cy=atof(argv[1]);
-      argc--; argv++;
-      cz=atof(argv[1]);
-      argc--; argv++;
-      }
-
-    if ((ok == false) && (strcmp(argv[1], "-res") == 0))
-      {
-      argc--; argv++;
-      ok = true;
-      sx=atof(argv[1]);
-      argc--; argv++;
-      sy=atof(argv[1]);
-      argc--; argv++;
-      }
-
-    if ((ok == false) && (strcmp(argv[1], "-size") == 0))
-      {
-      argc--; argv++;
-      ok = true;
-      dx=atoi(argv[1]);
-      argc--; argv++;
-      dy=atoi(argv[1]);
-      argc--; argv++;
-      }
-
-    if ((ok == false) && (strcmp(argv[1], "-sid") == 0))
-      {
-      argc--; argv++;
-      ok = true;
-      sid=atof(argv[1]);
-      argc--; argv++;
-      }
-
-    if ((ok == false) && (strcmp(argv[1], "-normal") == 0))
-      {
-      argc--; argv++;
-      ok = true;
-      o2Dx=atof(argv[1]);
-      argc--; argv++;
-      o2Dy=atof(argv[1]);
-      argc--; argv++;
-      }
-
-    if ((ok == false) && (strcmp(argv[1], "-o") == 0))
-      {
-      argc--; argv++;
-      ok = true;
-      output_name = argv[1];
-      argc--; argv++;
-      }
-
-    if (ok == false) 
-      {
-
-      if (input_name == NULL) 
-        {
-        input_name = argv[1];
-        argc--;
-        argv++;
-        }
-      
-      else 
-        {
-        std::cerr << "ERROR: Can not parse argument " << argv[1] << std::endl;
-        usage();
-        }
-      }
-    } 
-
-  if (verbose) 
-    {
-    if (input_name)  std::cout << "Input image: "  << input_name  << std::endl;
-    if (output_name) std::cout << "Output image: " << output_name << std::endl;
-    }
-
+  string input_name;
+  string output_dir;
+  if (argc == 3) {
+    input_name = argv[1];
+    output_dir = argv[2];
+  }
 
   const     unsigned int   Dimension = 3;
+  const     unsigned int   OutDimension = 2;
   typedef short InputPixelType;
   typedef int FilterPixelType;
   typedef itk::Image< InputPixelType,  Dimension >   InputImageType;
   typedef itk::Image< FilterPixelType, Dimension >   FilterImageType;
+  typedef itk::Image< FilterPixelType, OutDimension >   OutFilterImageType;
 
   InputImageType::Pointer image;
+  itk::MetaDataDictionary dict;
 
 
-  if (input_name) 
+  if (input_name.size() && output_dir.size()) 
     {
       if (boost::filesystem::is_regular_file( input_name )) {
 	typedef itk::ImageFileReader< InputImageType >  ReaderType;
@@ -251,8 +51,8 @@ int main( int argc, char *argv[] )
 	  std::cerr << err << std::endl; 
 	  return EXIT_FAILURE;
 	  } 
-
 	image = reader->GetOutput();
+	dict = reader->GetMetaDataDictionary();
       } else if (boost::filesystem::is_directory( input_name )) {
         itkBasic::SeriesReader sreader( input_name );
 	sreader.readSeriesData( 2 );
@@ -262,6 +62,7 @@ int main( int argc, char *argv[] )
 	    itkBasic::FileNamesContainer fc;
 	    sreader.getSeriesFileNames(0, fc);
 	    image = itkBasic::getDicomSerie( fc, imageReader, 1 ); 
+	    dict = *((*imageReader->GetMetaDataDictionaryArray())[0]);
 	}
 	catch( itk::ExceptionObject & err ) 
 	  { 
@@ -269,197 +70,97 @@ int main( int argc, char *argv[] )
 	  std::cerr << err << std::endl; 
 	  return EXIT_FAILURE;
 	  } 
-      } else {
-	std::cerr << "input is neither dir nor file - please go away!" << std::endl;
-	usage();
       }
+    }
+    
+    if (!image) {
+	std::cerr << argv[0] << ": input output" << std::endl;
+	exit(1);
     }
   
-  // Print out the details of the input volume
-
-  if (verbose) 
-    {
-    unsigned int i;
-    const InputImageType::SpacingType spacing = image->GetSpacing();  
-    std::cout << std::endl << "Input ";
-    
-    InputImageType::RegionType region = image->GetBufferedRegion();
-    region.Print(std::cout);
-    
-    std::cout << "  Resolution: [";
-    for (i=0; i<Dimension; i++) 
-      {
-      std::cout << spacing[i];
-      if (i < Dimension-1) std::cout << ", ";
-      }
-    std::cout << "]" << std::endl;
-    
-    const InputImageType::PointType origin = image->GetOrigin();
-    std::cout << "  Origin: [";
-    for (i=0; i<Dimension; i++) 
-      {
-      std::cout << origin[i];
-      if (i < Dimension-1) std::cout << ", ";
-      }
-    std::cout << "]" << std::endl<< std::endl;
-    }
-
-  typedef itk::ResampleImageFilter<FilterImageType, FilterImageType > FilterType;
-
-  FilterType::Pointer filter = FilterType::New();
 
   typedef itk::SigmoidImageFilter< InputImageType, FilterImageType > SigmoidCasterType;
   SigmoidCasterType::Pointer sigmoidcaster = SigmoidCasterType::New();
+  
   sigmoidcaster->SetInput( image );
   sigmoidcaster->SetOutputMaximum( 4000 );
   sigmoidcaster->SetOutputMinimum( 1000 );
 
-  filter->SetInput( sigmoidcaster->GetOutput() );
-  filter->SetDefaultPixelValue( -1000 );
+  
+  typedef itk::AccumulateImageFilter< FilterImageType, FilterImageType > AccumulateFilter;
+  AccumulateFilter::Pointer accumulator = AccumulateFilter::New();
+  accumulator->SetAccumulateDimension(1);
+  accumulator->SetInput( sigmoidcaster->GetOutput() );
 
-  typedef itk::CenteredEuler3DTransform< double >  TransformType;
+  typedef itk::ExtractImageFilter< FilterImageType, OutFilterImageType > ExtractFilter;
+  ExtractFilter::Pointer extractor = ExtractFilter::New();
+  extractor->SetInput( accumulator->GetOutput() );
+  FilterImageType::Pointer accuOut = accumulator->GetOutput();
+  accuOut->UpdateOutputInformation();
+  FilterImageType::RegionType extractRegion = accuOut->GetLargestPossibleRegion();
+  
+  extractRegion.SetSize(1,0);
+  
+  extractor->SetExtractionRegion( extractRegion );
 
-  TransformType::Pointer transform = TransformType::New();
-
-  transform->SetComputeZYX(true);
-
-  TransformType::OutputVectorType translation;
-
-  translation[0] = tx;
-  translation[1] = ty;
-  translation[2] = tz;
-
-  // constant for converting degrees into radians
-  const double dtr = ( vcl_atan(1.0) * 4.0 ) / 180.0;
-
-  transform->SetTranslation( translation );
-  transform->SetRotation( dtr*rx, dtr*ry, dtr*rz );
-
-  InputImageType::PointType   imOrigin = image->GetOrigin();
-  InputImageType::SpacingType imRes    = image->GetSpacing();
-
-  typedef InputImageType::RegionType     InputImageRegionType;
-  typedef InputImageRegionType::SizeType InputImageSizeType;
-
-  InputImageRegionType imRegion = image->GetBufferedRegion();
-  InputImageSizeType   imSize   = imRegion.GetSize();
-
-  imOrigin[0] += imRes[0] * static_cast<double>( imSize[0] ) / 2.0; 
-  imOrigin[1] += imRes[1] * static_cast<double>( imSize[1] ) / 2.0; 
-  imOrigin[2] += imRes[2] * static_cast<double>( imSize[2] ) / 2.0; 
-
-  TransformType::InputPointType center;
-  center[0] = cx + imOrigin[0];
-  center[1] = cy + imOrigin[1];
-  center[2] = cz + imOrigin[2];
-
-  transform->SetCenter(center);
-
-  if (verbose) 
-    {
-    std::cout << "Image size: "
-              << imSize[0] << ", " << imSize[1] << ", " << imSize[2] << std::endl
-              << "   resolution: "
-              << imRes[0] << ", " << imRes[1] << ", " << imRes[2] << std::endl
-              << "   origin: "
-              << imOrigin[0] << ", " << imOrigin[1] << ", " << imOrigin[2] << std::endl
-              << "   center: "
-              << center[0] << ", " << center[1] << ", " << center[2] << std::endl
-              << "Transform: " << transform << std::endl;
-    }
-
-
-  typedef itk::RayCastInterpolateImageFunction<FilterImageType,double> InterpolatorType;
-
+  typedef itk::ResampleImageFilter<OutFilterImageType, OutFilterImageType > ResampleFilter;
+  ResampleFilter::Pointer resampler = ResampleFilter::New();
+  resampler->SetInput( extractor->GetOutput() );
+  
+  typedef itk::BSplineInterpolateImageFunction< OutFilterImageType > InterpolatorType;
   InterpolatorType::Pointer interpolator = InterpolatorType::New();
+  interpolator->SetSplineOrder(3);
+  
+  resampler->SetInterpolator( interpolator );
+  OutFilterImageType::Pointer exOut = extractor->GetOutput();
+  exOut->UpdateOutputInformation();
+  
+  typedef itk::CenteredRigid2DTransform< double > TransformType;
+  TransformType::Pointer transform = TransformType::New();
+  transform->SetIdentity();
+  OutFilterImageType::PointType exOutCenter = exOut->GetOrigin();
+  exOutCenter[0] += (exOut->GetLargestPossibleRegion().GetSize()[0]-1) * exOut->GetSpacing()[0] *.5;
+  exOutCenter[1] += (exOut->GetLargestPossibleRegion().GetSize()[1]-1) * exOut->GetSpacing()[1] *.5;
+  transform->SetCenter( exOutCenter );
+  transform->SetAngleInDegrees( 180 );
+  resampler->SetTransform( transform );
+  resampler->SetOutputParametersFromImage( exOut );
 
-  interpolator->SetTransform(transform);
+  OutFilterImageType::SpacingType resampleSpacing = exOut->GetSpacing();
+  resampleSpacing.Fill( std::min( resampleSpacing[0], resampleSpacing[1] ) );
+  OutFilterImageType::SizeType resampleSize;
+  resampleSize[0] = exOut->GetLargestPossibleRegion().GetSize()[0] * exOut->GetSpacing()[0] / resampleSpacing[0];
+  resampleSize[1] = exOut->GetLargestPossibleRegion().GetSize()[1] * exOut->GetSpacing()[1] / resampleSpacing[1];
+  resampler->SetSize( resampleSize );
+  resampler->SetOutputSpacing( resampleSpacing );
+  
+  OutFilterImageType::Pointer result = resampler->GetOutput();
+  
+  sigmoidcaster->SetBeta( -500 );
+  sigmoidcaster->SetAlpha( 5 );
+  result->Update();
 
-  interpolator->SetThreshold(threshold);
-  InterpolatorType::InputPointType focalpoint;
+  int outDicomIndex = 0;
+  itk::EncapsulateMetaData( dict, "0008|0008", string("DERIVED\\SECONDARY\\AXIAL"));
+  
+  boost::filesystem::path outpath = output_dir;
+  outpath = outpath / "IM%06d";
+  
+  std::vector< itk::MetaDataDictionary* > dictArray;
+  dictArray.push_back(&dict);
+  
+  itkBasic::writeDicomSeries( itkBasic::ImageRescale(itkBasic::ImageSharp(result, 0.5), -1000, 4000), outpath.string(), &dictArray, outDicomIndex);
+//  itkBasic::ImageSave( itkBasic::ImageSharp(result, 0.5), boost::str( boost::format("%s.%s.png") % output_name % "lung" ), 1, 0); // Auto Level
 
-  focalpoint[0]= imOrigin[0];
-  focalpoint[1]= imOrigin[1];
-  focalpoint[2]= imOrigin[2] - sid/2.;
-
-  interpolator->SetFocalPoint(focalpoint);
-
-  if (verbose)
-    {
-    std::cout << "Focal Point: " 
-              << focalpoint[0] << ", " 
-              << focalpoint[1] << ", " 
-              << focalpoint[2] << std::endl;
-    }
-
-  filter->SetInterpolator( interpolator );
-  filter->SetTransform( transform );
-
-  // setup the scene
-  InputImageType::SizeType   size;
-
-  size[0] = dx;  // number of pixels along X of the 2D DRR image 
-  size[1] = dy;  // number of pixels along Y of the 2D DRR image 
-  size[2] = 1;   // only one slice
-
-  filter->SetSize( size );
-
-  InputImageType::SpacingType spacing;
-
-  spacing[0] = sx;  // pixel spacing along X of the 2D DRR image [mm]
-  spacing[1] = sy;  // pixel spacing along Y of the 2D DRR image [mm]
-  spacing[2] = 1.0; // slice thickness of the 2D DRR image [mm]
-
-  filter->SetOutputSpacing( spacing );
-
-  if (verbose) 
-    {
-    std::cout << "Output image size: " 
-              << size[0] << ", " 
-              << size[1] << ", " 
-              << size[2] << std::endl;
-    
-    std::cout << "Output image spacing: " 
-              << spacing[0] << ", " 
-              << spacing[1] << ", " 
-              << spacing[2] << std::endl;
-    }
-
-  double origin[ Dimension ];
-
-  origin[0] = imOrigin[0] + o2Dx - sx*((double) dx)/2.; 
-  origin[1] = imOrigin[1] + o2Dy - sy*((double) dy)/2.; 
-  origin[2] = imOrigin[2] + sid/2.; 
-
-  filter->SetOutputOrigin( origin );
-
-  if (verbose) 
-    {
-    std::cout << "Output image origin: " 
-              << origin[0] << ", " 
-              << origin[1] << ", " 
-              << origin[2] << std::endl;
-    }
-
-  // create writer
-
-  if (output_name) 
-    {
-    FilterImageType::Pointer result = filter->GetOutput();
-    
-    sigmoidcaster->SetBeta( -500 );
-    sigmoidcaster->SetAlpha( 5 );
-    result->Update();
-    itkBasic::ImageSave( itkBasic::ImageSharp(result, 0.1), boost::str( boost::format("%s.%s.png") % output_name % "lung" ), 1, 0); // Auto Level
-
-    sigmoidcaster->SetBeta( 1000 );
-    sigmoidcaster->SetAlpha( 300 );
-    result->Update();
-    itkBasic::ImageSave( itkBasic::ImageSharp(result, 0.1), boost::str( boost::format("%s.%s.png") % output_name % "bone" ), 1, 0); // Auto Level
-    
-    sigmoidcaster->SetBeta( 0 );
-    sigmoidcaster->SetAlpha( 2000 );
-    result->Update();
-    itkBasic::ImageSave( itkBasic::ImageSharp(result, 0.1), boost::str( boost::format("%s.%s.png") % output_name % "normal" ), 1, 0); // Auto Level
-    }
+  sigmoidcaster->SetBeta( 1000 );
+  sigmoidcaster->SetAlpha( 300 );
+  result->Update();
+  itkBasic::writeDicomSeries( itkBasic::ImageRescale(itkBasic::ImageSharp(result, 0.5), -1000, 4000), outpath.string(), &dictArray, outDicomIndex);
+//  itkBasic::ImageSave( itkBasic::ImageSharp(result, 0.5), boost::str( boost::format("%s.%s.png") % output_name % "bone" ), 1, 0); // Auto Level
+  
+  sigmoidcaster->SetBeta( 0 );
+  sigmoidcaster->SetAlpha( 2000 );
+  result->Update();
+  itkBasic::writeDicomSeries( itkBasic::ImageRescale(itkBasic::ImageSharp(result, 0.5), -1000, 4000), outpath.string(), &dictArray, outDicomIndex);
+//  itkBasic::ImageSave( itkBasic::ImageSharp(result, 0.5), boost::str( boost::format("%s.%s.png") % output_name % "normal" ), 1, 0); // Auto Level
 }
